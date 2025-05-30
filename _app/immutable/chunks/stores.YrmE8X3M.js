dce140a2e37e144d3577ac33b330102ef9535c47
Giacomo Nanni <gn.nanni@gmail.com>
2025-05-30 16:54:37 +0200

    Updates

--- _app/immutable/chunks/stores.YrmE8X3M.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./entry.LSnk8_jK.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
--- _app/immutable/entry/start.CHFbTKF8.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.LSnk8_jK.js";export{t as start};
