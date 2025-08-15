fffe03264bc0217379aa79b1049672a08d44e525
Giacomo Nanni <gn.nanni@gmail.com>
2025-08-15 22:57:22 +0200

    svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-vercel";
 // import preprocess from 'svelte-preprocess';
-import { mdsvex } from 'mdsvex';
-import mdsvexConfig from './mdsvex.config.js';
-const dev = process.argv.includes('dev');
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
+const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
-	preprocess: [
-		mdsvex(mdsvexConfig)
-	],
-	kit: {
-		adapter: adapter(),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
-		},
-		alias: {
-			'@components': 'src/components',
-			'@stores': 'src/routes/stores.js',
-			'@utils': 'src/utils.js',
-		},
-	}
+  extensions: [".svelte", ...mdsvexConfig.extensions],
+  preprocess: [mdsvex(mdsvexConfig)],
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: dev ? "" : process.env.BASE_PATH,
+    },
+    alias: {
+      "@components": "src/components",
+      "@stores": "src/routes/stores.js",
+      "@utils": "src/utils.js",
+    },
+  },
 };
 
 export default config;
