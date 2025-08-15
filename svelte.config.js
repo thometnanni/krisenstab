2e064e41aad889376c5f25469d6f7a22829bce38
Giacomo Nanni <gn.nanni@gmail.com>
2025-08-15 23:05:49 +0200

    chore back

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,14 @@
-import adapter from "@sveltejs/adapter-vercel";
-// import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-auto";
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 const dev = process.argv.includes("dev");
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ...mdsvexConfig.extensions],
   preprocess: [mdsvex(mdsvexConfig)],
   kit: {
     adapter: adapter(),
-    paths: {
-      base: dev ? "" : process.env.BASE_PATH,
-    },
+    paths: { base: dev ? "" : process.env.BASE_PATH },
     alias: {
       "@components": "src/components",
       "@stores": "src/routes/stores.js",
@@ -20,5 +16,4 @@ const config = {
     },
   },
 };
-
 export default config;
