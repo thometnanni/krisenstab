e4d8e767602349e41fb3025548d8769be1431dd3
Giacomo Nanni <gn.nanni@gmail.com>
2025-10-28 10:41:14 +0100

    Updates

---
+++ _app/immutable/chunks/Bs2MYSNn.js
@@ -0,0 +1,1 @@
+import{s as e}from"./hE8STXE1.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
