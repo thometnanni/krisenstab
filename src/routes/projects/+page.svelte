9184fba079957ebac6870820453b1b593d12a3f1
Giacomo Nanni <gn.nanni@gmail.com>
2026-01-08 10:45:10 +0100

    chore paddings

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -38,7 +38,7 @@
   <meta name="twitter:image" content="https://krisenstab.net/og-image.png" />
 </svelte:head>
 
-<main class="mx-auto max-w-screen-3xl mt-2 px-3 text-black text-balance">
+<main class="mx-auto max-w-screen-3xl mt-2 px-2 text-black text-balance">
   <div class="p-container space-y-2">
     {#each projects as p (p.slug)}
       <div class="border-b border-gray-200 mb-2">
@@ -46,23 +46,23 @@
           <a href={`/projects/${p.slug}`} class="block">
             <Gallery>
               <div
-                class="sticky left-0 flex-shrink-0 max-w-[300px] h-full text-sm md:max-w-[350px] md:text-base bg-white"
+                class="sticky left-0 flex-shrink-0 max-w-[280px] h-full text-base bg-white"
               >
-                <div class="space-y-3 h-[350px] p-2">
+                <div class="space-y-3 h-[350px] px-2">
                   <div class="grid grid-cols-[auto,1fr] text-left">
                     <time
                       class="text-gray-500 text-sm [font-variant-numeric:tabular-nums]"
                       >{formatDate(p.date)}</time
                     >
                     <div class="text-black overflow-hidden">
-                      <div class="text-xl leading-[1.1] break-words">
+                      <div class="text-base leading-[1.1] break-words">
                         {p.title}
                       </div>
                     </div>
                   </div>
                   <div
                     class:opacity-0={!p.summaryHtml}
-                    class="content text-xl leading-[1.2] text-wrap"
+                    class="content text-base leading-[1.2] text-wrap"
                   >
                     {@html p.summaryHtml || ""}
                   </div>
