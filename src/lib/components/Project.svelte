f03eed52eb8c99ec56fa73019038d5aca6edccb8
Giacomo Nanni <gn.nanni@gmail.com>
2026-02-09 11:57:15 +0100

    fix distances

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -4,9 +4,9 @@
 	let { html, id, title, date, left, right, offset } = $props();
 </script>
 
-<article class="project my-12 flex flex-col items-center gap-12">
+<article class="project my-12 flex flex-col items-center gap-4">
 	<Cover {left} {right} {offset} />
-	<section class="mx-4 prose prose-xl mt-4 font-serif">
+	<section class="mx-4 prose prose-xl font-serif">
 		<h2>{title}</h2>
 		<section>{@html html}</section>
 	</section>
