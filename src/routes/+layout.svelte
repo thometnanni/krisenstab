36f00deda5c38ae3cb771d26ca01912347bfb465
Fidel Thomet <git@ft0.ch>
2026-02-10 12:15:43 +0100

    prevent history.scrollRestoration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,16 @@
 <script>
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.png';
+	import { onMount } from 'svelte';
 
 	let { children } = $props();
+
+	onMount(() => {
+		if ('scrollRestoration' in history) {
+			history.scrollRestoration = 'manual';
+			window.scrollTo(0, 0);
+		}
+	});
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
