779b45b390424b75a8efa4aef309417a1ad54a84
Fidel Thomet <git@ft0.ch>
2026-02-10 10:40:10 +0100

    link to project specific page from project title

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -1,13 +1,14 @@
 <script>
 	import Cover from './Cover.svelte';
+	import { resolve } from '$app/paths';
 
-	let { html, id, title, date, left, right, offset } = $props();
+	let { html, id, title, date, left, right, offset, name } = $props();
 </script>
 
 <article class="project my-12 flex flex-col items-center gap-5">
 	<Cover {left} {right} {offset} />
 	<section class="description mt-4b mx-4 prose prose-2xl font-serif">
-		<h2>{title}</h2>
+		<h2>{title}<a class="ml-1 text-xs" href={resolve(`/${name}`)}>↗</a></h2>
 		<section>{@html html}</section>
 	</section>
 </article>
