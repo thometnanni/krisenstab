c0d55899c4e7e50c532d0a82d91061434cfb9bef
Fidel Thomet <git@ft0.ch>
2026-02-12 12:30:10 +0100

    remove parallax

--- src/lib/components/Cover.svelte
+++ src/lib/components/Cover.svelte
@@ -3,11 +3,6 @@
 
 	let { left, right, offset = 0 } = $props();
 
-	let container = $state();
-	let parallax = $state(0);
-	let containerHeight = $state(0);
-	let rafId;
-
 	let leftHeight = $state(0);
 	let rightHeight = $state(0);
 	const offsets = $derived.by(() => {
@@ -15,50 +10,16 @@
 		const r = center + offset * 0.01 * Math.max(rightHeight, leftHeight) * 0.5;
 		return { left: Math.max(0, -r), right: Math.max(0, r) };
 	});
-
-	onMount(() => {
-		const observer = new IntersectionObserver(
-			(entries) => {
-				entries.forEach((entry) => {
-					if (entry.isIntersecting) {
-						if (!rafId) rafId = requestAnimationFrame(updateParallax);
-					} else {
-						if (rafId) {
-							cancelAnimationFrame(rafId);
-							rafId = null;
-						}
-					}
-				});
-			},
-			{ threshold: [0, 1] }
-		);
-		observer.observe(container);
-	});
-
-	function updateParallax() {
-		const rect = container.getBoundingClientRect();
-		const vh = window.innerHeight;
-
-		const travelled = 1 - (vh - rect.top) / (vh + rect.height);
-
-		parallax = offsets.left + leftHeight > offsets.right + rightHeight ? travelled : -travelled;
-		if (travelled > 0 && travelled < 1) {
-			rafId = requestAnimationFrame(updateParallax);
-		} else {
-			rafId = null;
-		}
-	}
 </script>
 
 {#if left && right}
-	<section class="grid grid-cols-2" bind:this={container}>
+	<section class="grid grid-cols-2">
 		<div
 			bind:clientHeight={leftHeight}
 			class="self-start justify-self-end"
 			style:margin-top="{offsets.left}px"
 			style:background={left.bg}
 			style:padding={left.padding?.map((p) => `calc(${p} * var(--passepartout))`).join(' ')}
-			style:transform="translate(0, {parallax * -100}px)"
 		>
 			<img loading="lazy" srcset="{left.src} {left.srcset ?? '2x'}" alt={left.alt} />
 		</div>
@@ -68,7 +29,6 @@
 			style:margin-top="{offsets.right}px"
 			style:background={right.bg}
 			style:padding={right.padding?.map((p) => `calc(${p} * var(--passepartout))`).join(' ')}
-			style:transform="translate(0, {parallax * 100}px)"
 		>
 			<img loading="lazy" srcset="{right.src} {right.srcset ?? '2x'}" alt={right.alt} />
 		</div>
