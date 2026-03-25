eda5ea19b106a83c8b802df4fe754a1bac1381e3
Giacomo Nanni <gn.nanni@gmail.com>
2026-03-25 12:13:44 +0100

    style: prominent links

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -21,7 +21,31 @@
 	}
 
 	:global(.project a) {
-		@apply link;
+		@apply link cursor-pointer text-[0.9em] text-[#ae24ff] no-underline;
+
+		&::before {
+			content: '✳';
+			padding-right: 1px;
+			font-size: 0.85em;
+			vertical-align: middle;
+			font-style: normal;
+		}
+
+		&::after {
+			content: '✳';
+			padding-left: 1px;
+			font-size: 0.85em;
+			vertical-align: middle;
+			font-style: normal;
+		}
+
+		&:hover {
+			@apply text-black;
+		}
+		&:hover::before,
+		&:hover::after {
+			@apply text-[#ae24ff];
+		}
 	}
 
 	:global(.project .prose p) {
@@ -34,18 +58,18 @@
 
 	@media (max-width: 640px) {
 		:global(.project .prose h2) {
-			@apply text-balance text-[clamp(1.75rem,1.4rem+1.9vw,2.25rem)] leading-[1.1] mb-3;
+			@apply mb-3 text-[clamp(1.75rem,1.4rem+1.9vw,2.25rem)] leading-[1.1] text-balance;
 		}
 
 		:global(.project .prose) {
-			@apply text-[clamp(1.02rem,0.96rem+0.45vw,1.14rem)] leading-[1.38]  max-w-[32ch];
+			@apply max-w-[32ch] text-[clamp(1.02rem,0.96rem+0.45vw,1.14rem)] leading-[1.38];
 			/* tracking-[0.005em] */
 			word-spacing: normal;
 			text-rendering: optimizeLegibility;
 		}
 
 		:global(.project .prose p) {
-			@apply hyphens-auto text-pretty break-words;
+			@apply text-pretty break-words hyphens-auto;
 			orphans: 2;
 			widows: 2;
 		}
@@ -54,5 +78,4 @@
 			@apply indent-[1.25em];
 		}
 	}
-	
 </style>
