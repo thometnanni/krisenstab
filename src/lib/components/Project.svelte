6e9d856f2e0443e40175c1f003d4e8805fde8774
Giacomo Nanni <gn.nanni@gmail.com>
2026-08-07 17:04:13 +0200

    chore 1.2em leading

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -9,7 +9,7 @@
 	<div class=" mt-40 mb-20 flex flex-col items-center text-center font-serif italic sm:my-40">
 		<p class="text-2xl font-normal sm:mb-2 sm:text-3xl sm:leading-none">{title}</p>
 		{#if description}
-			<p class="max-w-2xs text-xs leading-none italic sm:max-w-96 sm:text-base">{description}</p>
+			<p class="max-w-2xs text-xs leading-[1.2em] italic sm:max-w-96 sm:text-base">{description}</p>
 		{/if}
 	</div>
 </article>
